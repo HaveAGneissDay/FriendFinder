a3f4ae716ef9ae3779e553d664ee44a5924d52ab
David Cheng <dkcheng10@gmail.com>
2018-02-17 16:33:43 -0800

    initial commitment plus psudo code

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,20 @@
+app.post('/api/friends', function (req, res) {
+
+    //looping through each object in the friendsArray
+
+    // within each friend, we are getting the difference in answer scores
+
+}
+      // pushing the scores into the differenceArray
+
+    };
+
+    // looping through array finding the lowest score
+
+        // assigning index to the lowest score
+
+      }
+    }
+    // returning the friend with the lowest difference in score
+
+  });
