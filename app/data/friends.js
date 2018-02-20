fc583be4fce22f223f9bc0724a6fae6066e7273d
David Cheng <dkcheng10@gmail.com>
2018-02-19 18:44:21 -0800

    put in html routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-```json
+var friendArray = [
 {
   "name":"Ahmed",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
@@ -14,4 +14,7 @@
       4,
       1
     ]
+}, {
+    
 }
+]
