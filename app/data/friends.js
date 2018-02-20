96e5d14b033d20982884a128c86988057fa82393
David Cheng <dkcheng10@gmail.com>
2018-02-19 18:50:48 -0800

    populated friends array and export

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,6 +15,103 @@ var friendArray = [
       1
     ]
 }, {
-    
-}
+        "name": "Sam",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3,
+            1,
+            4,
+            3,
+            4,
+            3,
+            2,
+            5,
+            4,
+            5
+        ]
+
+    }, {
+        "name": "John",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3,
+            4,
+            4,
+            4,
+            4,
+            5,
+            2,
+            5,
+            5,
+            4
+        ]
+
+    }, {
+        "name": "Luke",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            2,
+            1,
+            4,
+            4,
+            5,
+            2,
+            2,
+            5,
+            3,
+            2
+        ]
+
+    },
+    {
+        "name": "Eve",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            2,
+            2,
+            4,
+            1
+        ]
+
+    }, {
+        "name": "Emily",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            1,
+            4,
+            3,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+
+    }, {
+        "name": "Lauren",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            5,
+            4,
+            4,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+
+    }
 ]
+
+module.exports = friendArray;
