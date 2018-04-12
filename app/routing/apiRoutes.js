7ca2dae4ec1c924005d3d02eea49e3d11172fd4c
David Cheng <dkcheng10@gmail.com>
2018-04-12 16:48:44 -0700

    This app is now working. All that is left is to clean up the code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,8 @@
 
-// Need Path
-var path = require('path');
-
 // Need friends array data
 var friends = require('../data/friends.js');
+
+function friend (app) {
 
 
 app.get('/api/friends', function (req, res) {
@@ -12,37 +11,49 @@ app.get('/api/friends', function (req, res) {
 
 var compatableFriends = [];
 app.post('/api/friends', function (req, res) {
-    var userInput = req.body;
-    var userResponses = userInput.scores;
-    var matchName = '';
-    var matchImage = '';
-    var totalDifference = 100;
+    var newFriend = req.body;
+    console.log('newFriend:', newFriend);
+    var newFriendResult = [];
         //looping through each object in the friendsArray
     for (var i = 0; i < friends.length; i++) {
         // console.log('friend = ' + JSON.stringify(friends[i]));
         var diff = 0;
-        for (var j = 0; j < userResponses.length; j++) {
+        for (var j = 0; j < 10; j++) {
             //need math abs in order to get the absolute value of scores
             // within each friend, we are getting the difference in answer scores
-            diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+            var userScoreCard = newFriend['scores[]']
+            var friendScoreCard = friends[i].scores;
+            diff += Math.abs(friends[i].scores[j] - userScoreCard[j]);
         }
+         newFriendResult.push(diff);
+    
+    }
 
-        if (diff < totalDifference) {
+        var min = Math.min.apply(null, newFriendResult);
+        for(var k =0; k < newFriendResult.length; k++) {
+
+        if (newFriendResult[k] == min) {
             // console.log('Closest match found = ' + diff);
             // console.log('Friend name = ' + friends[i].name);
             // console.log('Friend image = ' + friends[i].photo);
 
     // looping through array finding the lowest score
         // assigning index to the lowest score
-            totalDifference = diff;
-            matchName = friends[i].name;
-            matchImage = friends[i].photo;
+            var matchName = friends[k].name;
+            var matchImage = friends[k].photo;
+
+            var bestFriend = {
+                newFriendName: matchName,
+                newFriendPhoto: matchImage
+            }
+            res.send(bestFriend);
         }
     }
-    compatableFriends.push(userInput);
+    // compatableFriends.push(userInput);
 
     // Send appropriate response
-    res.json({ status: 'OK', matchName: matchName, matchImage: matchImage });
-});
+    // res.json({ status: 'OK', matchName: matchName, matchImage: matchImage });
 
-module.exports = compatableFriends;
+});
+};
+module.exports = friend;
--- server.js
+++ server.js
@@ -4,15 +4,13 @@ var bodyParser = require("body-parser");
 var path = require("path");
 // Set up express
 var app = express();
+//PORT
 var PORT = process.env.PORT || 3000;
 
 //Server
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
-
-//Data
-var friendsArray = [];
 
 //Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,5 +21,5 @@ app.use(express.static(path.join(__dirname, "/app/public")));
 
 
 // Add the application routes
-var apiRoutes = require(path.join(__dirname, './app/routing/apiRoutes'));
-var htmlRoutes = require(path.join(__dirname, './app/routing/htmlRoutes'));
+require( './app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
